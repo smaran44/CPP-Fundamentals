5585caeee3c610ed5547de95904ad860aaa9f7f3
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:44:41 +0530

    Create 01_basic_structure.cpp

---
+++ 01 Variables, Data Types & Operators/01_basic_structure.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // 1️⃣ Header file for input/output
+using namespace std; // 2️⃣ Standard namespace
+
+int main() {         // 3️⃣ Main function
+    cout << "Hello, World!";  // 4️⃣ Output statement
+    return 0;        // 5️⃣ End of program
+}
