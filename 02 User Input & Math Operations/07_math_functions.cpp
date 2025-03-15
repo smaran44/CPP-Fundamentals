a7a8186a4da284fd3cc1498f6d6bb7c243f67886
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:49:54 +0530

    Create 07_math_functions.cpp

---
+++ 02 User Input & Math Operations/07_math_functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>  // Include cmath for math functions
+using namespace std;
+
+int main() {
+    double num = 25.0;
+
+    cout << "Square root of 25: " << sqrt(num) << endl;
+    cout << "2 raised to power 3: " << pow(2, 3) << endl;
+    cout << "Absolute value of -10: " << abs(-10) << endl;
+    cout << "Round 4.7: " << round(4.7) << endl;
+    cout << "Ceil of 4.1: " << ceil(4.1) << endl;
+    cout << "Floor of 4.9: " << floor(4.9) << endl;
+
+    return 0;
+}
