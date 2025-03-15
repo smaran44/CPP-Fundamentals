411dfd40bd28f24db0e21f6cd3edae1445f3c23c
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:45:40 +0530

    Create 07_variables.cpp

---
+++ 01 Variables, Data Types & Operators/07_variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;          // Integer variable
+    double pi = 3.14159;   // Floating point variable
+    char grade = 'A';      // Character variable
+    bool isStudent = true; // Boolean variable
+
+    // Printing the values
+    cout << "Age: " << age << endl;
+    cout << "Value of Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student? " << isStudent << endl;
+
+    return 0;
+}
