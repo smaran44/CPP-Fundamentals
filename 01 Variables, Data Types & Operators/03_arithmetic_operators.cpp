1ada2760dfc10431f97da53d9c92a1f68a4da19b
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:44:54 +0530

    Create 03_arithmetic_operators.cpp

---
+++ 01 Variables, Data Types & Operators/03_arithmetic_operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+    cout << "Addition: " << (a + b) << endl;
+    cout << "Subtraction: " << (a - b) << endl;
+    cout << "Multiplication: " << (a * b) << endl;
+    cout << "Division: " << (a / b) << endl;
+    cout << "Modulus: " << (a % b) << endl;
+
+    return 0;
+}
