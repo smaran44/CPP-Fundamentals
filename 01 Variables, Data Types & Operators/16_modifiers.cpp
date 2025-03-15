f99812dd163a56ecb3b951f351c669147ff76dab
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 21:51:38 +0530

    Create 16_modifiers.cpp

---
+++ 01 Variables, Data Types & Operators/16_modifiers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int a = 100;   // Only positive values
+    signed int b = -50;     // Both negative and positive
+    long int c = 999999999; // Large integer
+
+    cout << "Unsigned Int: " << a << endl;
+    cout << "Signed Int: " << b << endl;
+    cout << "Long Int: " << c << endl;
+
+    return 0;
+}
