712bf25118bf900d8763527e2f053d69042fb9a8
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:50:48 +0530

    Create 01_while_loop_example.cpp

---
+++ 04 Iteration & Loop Control Structure/01_while_loop_example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 1;
+    
+    while (count <= 5) {  
+        cout << "Count: " << count << endl;
+        count++;
+    }
+
+    return 0;
+}
