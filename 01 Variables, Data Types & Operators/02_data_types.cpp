d08a08cf1e9f435f323649325cdaa70cc7d44207
Smaran Yanapu <smaranyanapu@gmail.com>
2025-03-15 14:44:48 +0530

    Create 02_data_types.cpp

---
+++ 01 Variables, Data Types & Operators/02_data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;         // Integer
+    float pi = 3.14;    // Float
+    double gravity = 9.81; // Double
+    char grade = 'A';   // Character
+    bool isPassed = true; // Boolean
+    string name = "John"; // String
+
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << pi << endl;
+    cout << "Double: " << gravity << endl;
+    cout << "Character: " << grade << endl;
+    cout << "Boolean: " << isPassed << endl;
+    cout << "String: " << name << endl;
+
+    return 0;
+}
